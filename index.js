af1c9ae80f13bdeee53e909e89e03bda7c255ecf
Sharat M R <cosmosgenius@gmail.com>
2014-08-11 08:29:24 +0530

    corrected intentation

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ var readline = require('readline');
 var t9 = require('./src');
 
 var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 rl.question('Enter t9 value?\n', function(answer) {
-  console.log('Decoded value is :', t9(answer));
-  rl.close();
+    console.log('Decoded value is :', t9(answer));
+    rl.close();
 });
